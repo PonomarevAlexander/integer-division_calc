c9cda18dee550176eaccb75c3124660ea6052780
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2020-12-17 02:03:11 +0100

    Main class imlemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
-
 public class Main {
 
     public static void main(String[] args) {
-        
-
+       Division div = new Division();
+       String result = div.makeDivision(512, 8);
+       
+       System.out.println(result);
     }
-
 }
